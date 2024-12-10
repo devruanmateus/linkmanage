22581181c03959afcf0ec97055638c4f2b71d8ad
ruanm <ruanmateus344@gmail.com>
2024-12-10 17:19:46 -0300

    Update LinkManage

---
+++ script.js
@@ -0,0 +1,8 @@
+let iconMenu = document.getElementById("icon-menu");
+let navigationMobile = document.querySelector(".navigation-vmobile");
+
+navigationMobile.classList.add("hidden");
+
+function openMenu() {
+  navigationMobile.classList.toggle("hidden");
+}
