4c2f71ca6c4426cd567e48cab320c40862379c53
ruanm <ruanmateus344@gmail.com>
2024-12-27 16:14:40 -0300

    Update
    A página "Sobre" foi criada, reajustes na posição e estilo do conteúdo e página de "Planos" sendo desenvolvida.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Abrir e fechar menu
 let iconMenu = document.getElementById("icon-menu");
 let navigationMobile = document.querySelector(".navigation-vmobile");
 
@@ -6,3 +7,27 @@ navigationMobile.classList.add("hidden");
 function openMenu() {
   navigationMobile.classList.toggle("hidden");
 }
+
+// Abrir e fechar resposta da faq
+const faqs = document.querySelectorAll(".faqs");
+
+faqs.forEach((faq) => {
+  const openIcon = faq.querySelector(".open-icon");
+  const closeIcon = faq.querySelector(".close-icon");
+  const reply = faq.querySelector(".reply");
+
+  closeIcon.classList.add("hidden");
+  reply.classList.add("hidden");
+
+  openIcon.addEventListener("click", () => {
+    reply.classList.remove("hidden");
+    openIcon.classList.add("hidden");
+    closeIcon.classList.remove("hidden");
+  });
+
+  closeIcon.addEventListener("click", () => {
+    reply.classList.add("hidden");
+    closeIcon.classList.add("hidden");
+    openIcon.classList.remove("hidden");
+  });
+});
